60a1350fd83c5a0c18a65ab896296eaadacf973b
Artem Ostretsov <OstretsovAA@gmail.com>
2018-04-25 17:16:58 +0300

    1.10

--- src/i_introduction/_9_Extension_Functions/JavaCode9.java
+++ src/i_introduction/_9_Extension_Functions/JavaCode9.java
@@ -2,7 +2,8 @@ package i_introduction._9_Extension_Functions;
 
 import util.JavaCode;
 
-public class JavaCode9 extends JavaCode {
+public class JavaCode9 extends JavaCode
+{
     public void useExtension() {
         char c = N09ExtensionFunctionsKt.lastChar("abc");
     }
