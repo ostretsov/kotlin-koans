c238096662207c5d6c2c9a2ace2def076d9870f9
Artem Ostretsov <OstretsovAA@gmail.com>
2018-04-27 14:52:34 +0300

    3.28

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -8,7 +8,7 @@ fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    return orders.filter { it.isDelivered == true }.flatMap { it.products }.maxBy { it.price }
+    return orders.filter { it.isDelivered }.flatMap { it.products }.maxBy { it.price }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,15 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import com.sun.org.apache.xpath.internal.operations.Bool
+import iv_properties.toMillis
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return (this.toMillis()/1000).toInt() - (other.toMillis()/1000).toInt()
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +17,19 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = object : Iterator<MyDate> {
+        var current = start
+
+        override fun next(): MyDate {
+            val result = current
+            current = current.nextDay()
+
+            return result
+        }
+
+        override fun hasNext() = current <= endInclusive
+    }
+
+    operator fun contains(date: MyDate): Boolean = start <= date && date <= endInclusive
+}
